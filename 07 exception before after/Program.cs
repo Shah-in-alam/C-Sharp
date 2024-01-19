566a53ff5f81bb495f2022a2df22a613d9b77a29
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-19 23:57:54 +0100

    Add files via upload

---
+++ 07 exception before after/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07_exception_before_after
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //old way handelling expetion
+           /* Console.Write("Enter a file name:");
+            string file= Console.ReadLine();
+            Console.Write("Enter a lines:");
+            int linner;
+            if(!Int32.TryParse(Console.ReadLine(), out linner))
+            {
+                Console.WriteLine("Not valid linner! we stop");
+                return;//returns input to the users
+            }
+            if(File.Exists(file))
+            {
+                string[] text = File.ReadAllLines(file);
+                if(linner>text.Length)
+                {
+                    Console.WriteLine("Linner is big");
+                    return;
+                    
+                }
+                Console.WriteLine(text[linner]);
+            }
+            else
+            {
+                Console.WriteLine("File does not exists;");
+                return;
+            }*/
+           // New way --> with exception
+            
+        }
+    }
+}
---
+++ atm/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _03_ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxAttempts = 3;
+            int attempts = 0;
+            int correctPin = Convert.ToInt32(Console.ReadLine()); // Read the correct PIN as the first input.
+
+            while (attempts < maxAttempts)
+            {
+               // Console.WriteLine($"Enter your correct PIN:{enteredPin}");
+                //Console.WriteLine("¶");
+                int enteredPin = Convert.ToInt32(Console.ReadLine());
+
+                if (enteredPin == correctPin)
+                {
+                    //Console.WriteLine($"Enter your correct PIN:{enteredPin}");
+                    //Console.WriteLine($"Enter your PIN to withdraw money:{enteredPin}");
+                    Console.WriteLine("Successful withdrawal");
+                    break;
+                }
+                else
+                {
+                    attempts++;
+                    if (attempts < maxAttempts)
+                    {   
+                        Console.WriteLine($"Enter your correct PIN: ");
+                        Console.WriteLine($"Enter your PIN to withdraw money: ");
+                        Console.WriteLine($"Enter your PIN again: ");
+                        Console.WriteLine($"Enter your PIN again: ");
+
+                        //Console.WriteLine("Enter your PIN to withdraw money: ");
+                        // Console.WriteLine("¶");
+
+                    }
+                }
+            }
+
+            if (attempts == maxAttempts)
+            {
+                Console.WriteLine("Too Many attempts");
+            }
+        }
+    }
+}
