3ea8fd81e7fefb9a5f700b4e347ee3a9dd926d11
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-19 22:36:10 +0100

    first commit

---
+++ 01-Uppercase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _01_Uppercase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("What's your first name? ");
+            string name = Console.ReadLine();
+            name = name.ToUpper();
+            Console.WriteLine($" HELLO {name}");
+        }
+    }
+}
