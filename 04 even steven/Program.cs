6c9a317fc54e29dd3107db35217d10aa3a75bd39
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-19 23:15:38 +0100

    c sharp assignment

---
+++ 04 even steven/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace _04_even_steven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end;
+               
+            if (int.TryParse(Console.ReadLine(), out start) && int.TryParse(Console.ReadLine(), out end))
+            {
+                long answer = 1;
+                for(int i = start;i <= end; i++)
+                {
+                    if(i % 2 == 0 )
+                    {
+                        answer *= i;
+                    }
+                }
+                Console.WriteLine(answer);
+            }
+            else
+            {
+             Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
