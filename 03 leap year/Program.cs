08bb928ceda89866ee15b95245610e3f01adb0b5
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-19 22:41:23 +0100

    c sharp assignment

---
+++ 03 leap year/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace _03_leap_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if(year %400 == 0)
+            {
+                Console.WriteLine($"{year} is a leap year");
+            }
+            else if(year %4 == 0 && year % 100!=0)
+            {
+                Console.WriteLine($"{year} is a leap year");
+            }
+            else
+            {
+               Console.WriteLine($"{year} is not a leap year");
+            }
+        }
+    }
+}
