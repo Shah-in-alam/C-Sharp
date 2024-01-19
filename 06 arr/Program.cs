805cb4157858e7276f33e54d0b645a7fc65c7f75
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-19 23:49:57 +0100

    Add files via upload

---
+++ 06 arr/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _06_arr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 12, 8, -3, 15, 20, 0, 78, -15, -6, 30 };
+            //get sum,min.
+            //average
+            Console.WriteLine(" sum: " + array.Sum());
+            Console.WriteLine(" min: " + array.Min());
+
+            Console.WriteLine(" sum: " + array.Average());
+            Array.Sort(array);
+            Array.Reverse(array);
+            foreach( var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            array = new int[] { 12, -3, 1,20,0,78,-15 -6, 30 };
+            //sum
+            int sum = 0;
+            for(int i =0; i<array.Length; i++)
+            {
+                sum += array[i];//summ= sum+array[i]
+                Console.WriteLine("Sum: " + sum);
+                Console.WriteLine("average: "+ sum/array.Length);
+
+                //find minum
+                int min = array[0];
+                foreach( var item in array)
+                {
+                    if (item < min)
+                    {
+                        min = item;
+                    }
+                }
+                Console.WriteLine("min: "+min);
+                // sorting algoriith
+                //
+                //check from start , each adgestment --if 2nd is 
+                for(int i=0; i<array.Length; i++)
+                {// checks multiple times
+                    for( int j =0;i<array.Length-1; j++) {
+                        //check the array
+                        if (array[j] > array[j+1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            //print array
+                            foreach( var item in array)
+                            {
+                                Console.WriteLine("item:" + item);
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ 06 array 2D/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace _06_array_2D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2d array
+
+            int[,] ints = new int[4, 5];// = [{ 1,2}{}[,
+            string[,] fruit = { { "apple", " pinapple", "lemon", "orange" },
+                {"mango", "lime","banana", "watermelon" },{"pieach","grapefriut","strawberay","kiwi" } };
+            //get values
+            //keep in mind that we start at[0,0]
+            Console.WriteLine($"element of row 1 col 1 :{fruit[0,0]}");
+            Console.WriteLine($"element of row 2 col 1 : {fruit[1,0]}");
+            fruit[1, 0] = "pear";
+            Console.WriteLine($"element of row 2 col 1: {fruit[1, 0]}");
+            //loop through array
+            Console.WriteLine(fruit.Length);
+            // length of all element
+            Console.WriteLine("#rows :"+ fruit.GetLength(0) );
+            Console.WriteLine("#colms :" + fruit.GetLength(1));
+            //loop througth
+            for( int i = 0; i < fruit.GetLength(0); i++ )
+            {
+                for( int j = 0; j < fruit.GetLength(1); j++)
+                {
+                    Console.WriteLine(fruit[i, j] + "\t");
+                }
+              //  Console.WriteLine();
+            }
+            Console.WriteLine();
+            //random
+            Random rd = new Random();
+            for( int i =0; i<ints.GetLength(0); i++)
+            {
+                for( int j =0; j<ints.GetLength(1); j++)
+                {
+                    ints[i,j];
+                }
+            }
+        }
+    }
+    
+}
