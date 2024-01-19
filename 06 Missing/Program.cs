58ac60b7c10182a130aeb14634c7d2f2366da146
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-19 23:54:27 +0100

    Add files via upload

---
+++ 06 Missing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+        int[] array = new int[numbers.Length];
+        int index = 0;
+        foreach (var number in numbers)
+        {
+            array[index++] = int.Parse(number);
+        }
+
+        Console.WriteLine(FindMissingNumber(array));
+    }
+
+    static string FindMissingNumber(int[] array)
+    {
+        int n = array.Length;
+        int total = (n + 1) * (n + 2) / 2;  // The total sum of numbers from 1 to n+1
+
+        foreach (var number in array)
+        {
+            total -= number;
+        }
+
+        if (total > n)
+        {
+            return "There is no missing link!";
+        }
+        else
+        {
+            return total.ToString();
+        }
+    }
+}
---
+++ 06 ex  report card/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace _06_ex__report_card
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Number of students: ");
+            int students = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of tests : ");
+            int tests = Convert.ToInt32(Console.ReadLine());
+            double[,]reportCard = new double[students, tests];
+            for( int  i= 0;i<students;i++ )
+            {
+                for (int j = 0; j < tests; j++)
+                {
+                    Console.Write($"Grade of tests{j + 1}of students{i + 1}: ");
+                    reportCard[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+               
+            }
+            //average
+            for( int i= 0; i<students;i++ )
+            {
+                double sum = 0;
+                for( int j = 0;j < tests; j++)
+                {
+                    sum += reportCard[i, j];
+                    
+                }
+                Console.WriteLine(sum/tests);
+            }
+            // test
+            for( int  i =0;i<tests;i++ )
+            {
+                double sum  = 0;
+                for(int j =0;j < students; j++)
+                {
+                    sum += reportCard[i, j];
+                }
+                Console.WriteLine(sum/students);
+            }
+        }
+    }
+}
