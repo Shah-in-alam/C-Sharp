670e96498ab20f5747b3c6f9e6db838479572203
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 00:41:28 +0100

    Add files via upload

---
+++ 06 Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string input=Console.ReadLine();
+            string[] word = input.Split(" ");
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                char[] charArray= word[i].ToCharArray();
+                int left = 0;
+                int right = charArray.Length-1;
+                while(left < right)
+                {
+                    char temp= charArray[left];
+                    charArray[left] = charArray[right];
+                    charArray[right] = temp;
+                    left++;
+                    right--;
+                }
+                Console.Write(new string (charArray)+ " ");
+            }
+        }
+    }
+}
---
+++ 06 mising/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_mising
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_Number = Console.ReadLine();
+            int[] numbers = input_Number.Split(" ").Select(int.Parse).ToArray();
+
+            bool isValidInput = numbers.All(num => num >= 1 && num <= 9);
+
+            if (!isValidInput)
+            {
+                Console.WriteLine("Please enter numbers from 1 to 9.");
+                return;
+            }
+
+            int expectedSum = Enumerable.Range(1, 9).Sum();
+            int actualSum = numbers.Sum();
+            int missingNumber = expectedSum - actualSum;
+
+            Console.WriteLine(missingNumber == 0 ? "There is no missing link" : missingNumber.ToString());
+        }
+    }
+}
