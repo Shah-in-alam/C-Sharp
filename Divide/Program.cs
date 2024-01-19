3c7aa19da55f265350ef0a01b7e555e71fc39675
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 00:19:46 +0100

    Add files via upload

---
+++ Divide/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Divide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" ");
+            string  a = Console.ReadLine();
+            Console.Write(" ");
+            string  b = Console.ReadLine();
+           int num1 = int.Parse(a);
+            int num2 = int.Parse(b); 
+            if (num1 % num2== 0)
+            { 
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
---
+++ EV/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class program
+{
+    static void Main()
+    {
+
+
+        //Read an integer from the user
+       // Console.WriteLine("");
+        int number = int.Parse(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("True");
+        }
+      else
+        {
+            Console.Write(false);
+        }
+    }
+}
