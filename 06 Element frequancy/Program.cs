d58d6cfea33b69bc30c569bae10112d7daed3e09
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 00:40:09 +0100

    Add files via upload

---
+++ 06 Element frequancy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace _06_Element_frequancy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip= Console.ReadLine();
+            char[] charArray = ip.ToCharArray();
+            int[] count = new int[256];
+            for(int i=0; i<charArray.Length; i++)
+            {
+                char currentChar = charArray[i];
+                count[currentChar]++;
+            }
+            for(int i=0; i<count.Length; i++)
+            {
+                if (count[i] > 0)
+                {
+                    Console.WriteLine($"Character '{(char)i}': {count[i]} times");
+                }
+            }
+        }
+    }
+}
---
+++ 06 ending/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace _06_ending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string input= Console.ReadLine();
+            string[] strings = input.Split(" ");
+            int length = Math.Min(strings[0].Length, strings[1].Length);
+            string commonEnding =string.Empty;
+            for(int i=1; i<length; i++)
+            {
+                string end1 = strings[0].Substring(strings[0].Length -i);
+                string end2 = strings[1].Substring(strings[1].Length -i);
+
+                if(end1 == end2)
+                {
+                    commonEnding = end1;
+                }
+                
+            }
+            Console.WriteLine((string.IsNullOrEmpty(commonEnding) ? "None" : commonEnding));
+        }
+    }
+    
+}
