e6a25e66fc50b68886220c7ade242a2cff11fc12
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 20:05:26 +0100

    Add files via upload

---
+++ 08 find/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            string result;
+            List<double> matchingNumbers = input.Split(',')
+                .Select(part =>
+                {
+                    if (double.TryParse(part, out double num))
+                    {
+                        string numString = num.ToString();
+
+                        if (numString.Contains('0') && numString[0] != '0')
+                        {
+                            return num;
+                        }
+                    }
+                    return double.NaN;
+                })
+                .Where(num => !double.IsNaN(num))
+                .OrderBy(num => num)
+                .ToList();
+            result = string.Join(" ", matchingNumbers);
+            Console.WriteLine(result + " ");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("crazy input");
+        }
+
+    }
+}
---
+++ 08 out with odd/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _08_out_with_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ip = Console.ReadLine();
+                int[] num = ip.Split(' ').Select(int.Parse).ToArray();
+                Dictionary<int, int> count = new Dictionary<int, int>();
+                foreach (int i in num)
+                {
+                    if (count.ContainsKey(i))
+                    {
+                        count[i]++;
+                    }
+                    else
+                    {
+                        count[i] = 1;
+                    }
+                }
+                List<int> result = num.Where(i => count[i] % 2 == 0).ToList();
+                string finalResult = string.Join(" ", result);
+                Console.WriteLine(finalResult + " ");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("converting problem");
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("too big for this type");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("crazy input");
+            }
+        }
+    }
+}
