85ca97ffc5cf9c72b315cc47f8a3feb55cdc019b
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 20:02:14 +0100

    c sharp assignment

---
+++ 05 duplicated/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05_duplicated
+{
+    internal class Program
+    {
+        static string RemoveDuplicates(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            string result = input[0].ToString();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Char currentChar = input[i];
+                bool isDuplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (currentChar == input[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (isDuplicate)
+                {
+                    result += currentChar;
+                }
+            }
+            return result;
+
+        }
+        static void Mian(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = RemoveDuplicates(input);
+            Console.WriteLine(output);
+        }
+    }
+}
