00c3c735827caaf1ebe155bfa81338bf2ff04e40
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 20:10:20 +0100

    c sharp assignment

---
+++ 07 multification/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace _07_multification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dimention = Console.ReadLine().Split(" ");
+                var dimention1 = dimention[0].Split("x");
+                var dimention2 = dimention[1].Split("x");
+
+                int r1 = int.Parse(dimention[0]);
+                int cl1 = int.Parse(dimention[1]);
+                int r2 = int.Parse(dimention[0]);
+                int cl2 = int.Parse(dimention[1]);
+                if (cl1 != r2)
+                {
+                    Console.WriteLine("wrong dimentions");
+                    return;
+                }
+                int[,] matrix1 = new int[r1, cl1];
+                int[,] matrix2 = new int[r2, cl2];
+                int[,] result = new int[r1, cl2];
+
+                for(int i = 0; i < r1; i++)
+                {
+                    var row = Console.ReadLine().Split(" ");
+                    for(int j=0;j< cl1; j++)
+                    {
+                        matrix1[i, j] = int.Parse(row[j]);
+                    }
+                }
+                for (int i = 0; i < r2; i++)
+                {
+                    var row = Console.ReadLine().Split(' ');
+                    for (int j = 0; j < cl2; j++)
+                    {
+                        matrix2[i, j] = int.Parse(row[j]);
+                    }
+                }
+                for (int i=0;i< r1; i++)
+                {
+                    
+                    for(int j=0;j< cl2; j++)
+                    {
+                        for(int k=0;k< cl1; k++)
+                        {
+                            result[i, j] += matrix1[i, k] * matrix2[k, j];
+                        }
+                    }
+
+                }
+                for(int i=0;i< r1; i++)
+                {
+                    for(int j=0;j< cl2; j++)
+                    {
+                        Console.Write(result[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                    
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("converting problem");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("index out of range");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("tooo big for this type");
+            }
+            catch(Exception) {
+                Console.WriteLine("crazy input");
+            }
+        }
+    }
+}
---
+++ 07 product/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+
+namespace Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (!int.TryParse(Console.ReadLine(), out int N) || N <= 0)
+                {
+                    throw new FormatException();
+                    return;
+                }
+                if (N == 1)
+                {
+                    Console.WriteLine("1");
+                    return;
+                }
+
+                int result = 0;
+                for (int digit = 9; digit >= 2; digit--)
+                    while (N % digit == 0)
+                    {
+                        checked
+                        {
+                            result = result * 10 + digit;
+                        }
+                        N /= digit;
+                    }
+
+                if (N > 1)
+                {
+                    Console.WriteLine("No solution found.");
+                    return;
+                }
+
+                int reversedResult = 0;
+                while (result > 0)
+                {
+                    reversedResult = reversedResult * 10 + result % 10;
+                    result /= 10;
+                }
+
+                Console.WriteLine(reversedResult);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("too large for this type");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("converting problem");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("crazy input");
+            }
+        }
+    }
+}
