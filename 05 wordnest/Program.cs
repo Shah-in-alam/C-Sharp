82d9532f681eda50fae9f2987ff2eaf17417bed2
Shah-in-alam <137727697+Shah-in-alam@users.noreply.github.com>
2024-01-20 20:03:10 +0100

    Add files via upload

---
+++ 05 wordnest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _05_wordnest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string startingOrd=Console.ReadLine();
+          string  finalWord=Console.ReadLine();
+            int count = 0;
+            if(startingOrd !=finalWord) 
+            {
+                while (finalWord.Contains(startingOrd))
+                {
+                    int index= finalWord.IndexOf(startingOrd);  
+                    count++;
+                    finalWord= finalWord.Remove(index,startingOrd.Length);
+
+                }
+                int totalFound = count - 1;
+                Console.WriteLine(totalFound);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+        }
+    }
+}
---
+++ isogram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace isogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string loweer = name.ToLower();
+            bool iso = !Regex.IsMatch(loweer, @"(.).*\1");
+            if(iso)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False"); 
+            }
+        }
+    }
+}
